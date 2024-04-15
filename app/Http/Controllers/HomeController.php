4beccd74f4b6011d11a4c09fa9c3edb2cc026c5d
Vladimir Emelianov <klaytdeath@gmail.com>
2024-04-15 11:08:10 +0300

    3 steps. Alpha_bboard_version. Добавлена валидация данных, разграничение прав доступа, локализация ошибок и меню регистрации на русский язык. На этом базовый функционал готов.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+    private const BB_VALIDATOR = [
+        'title' => 'required|max:50',
+        'content' => 'required',
+        'price' => 'required|numeric',
+    ];
     /**
      * Create a new controller instance.
      *
@@ -37,9 +42,10 @@ class HomeController extends Controller
 
     public function store(Request $request)
     {
-        Auth::user()->bbs()->create(['title' => $request->title,
-            'content' => $request->content,
-            'price' => $request->price,]);
+        $validated = $request->validate(self::BB_VALIDATOR);
+        Auth::user()->bbs()->create(['title' => $validated['title'],
+                                     'content' => $validated['content'],
+                                     'price' => $validated['price'],]);
         return redirect()->route('home');
     }
 
@@ -50,9 +56,10 @@ class HomeController extends Controller
 
     public function update(Request $request, Bb $bb)
     {
-        $bb->fill(['title' => $request->title,
-            'content' => $request->content,
-            'price' => $request->price]);
+        $validated = $request->validate(self::BB_VALIDATOR);
+        $bb->fill(['title' => $validated['title'],
+                   'content' => $validated['content'],
+                   'price' => $validated['price']]);
         $bb->save();
         return redirect()->route('home');
     }
